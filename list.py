9848744cbcd1ddf72ad8bb07eab4c9be97f30bab
Sheep Core <leepaul168@gmail.com>
2019-04-12 23:00:17 +0800

    learning python for the first day

---
+++ list.py
@@ -0,0 +1,17 @@
+#1. create an integer list
+list = []
+
+#2. add some elements into the list
+list.append(1)
+list.append(2)
+list.append(4)
+
+#3. remove stored element
+list.remove(4)
+print("no.2 of list is %d" % list[1])
+
+#4. traverse elements in list
+for element in list:
+    print(element)
+
+
---
+++ operator.py
@@ -0,0 +1,26 @@
+#1. multiplications for square and cube
+squared = 2 ** 2
+cubed = 3 ** 3
+print("squared : %d, Cubed: %d" % (squared, cubed))
+
+#2. using + connect string
+s1 = 'I'
+s2 = "love"
+s3 = s1 + ' ' + s2 + ' ' + 'python.'
+print(s3)
+
+#3.1 multiplication for string, this is very funny to learn
+lotsOfLove = "love" * 9
+print(lotsOfLove)
+
+#3.2 multiplication for list
+list = [1, 2, 3] * 3
+print(list)
+
+#4. operators with lists :)
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print(list1 + list2)
+
+
+

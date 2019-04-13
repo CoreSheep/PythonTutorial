184dd739bd5868645ad91bf57fbd0779dd835224
Sheep Core <leepaul168@gmail.com>
2019-04-13 10:12:56 +0800

    Formatting of String like %d %f %s %x...

---
+++ StringFormatting.py
@@ -0,0 +1,17 @@
+#1. print "hello, SheepCore"
+name = "SheepCore"
+print("Hello, %s" %name)
+
+#2. group of string
+agender = "male"
+age = 22
+print("My name is %s, I'm %s with %d." %(name, agender, age))
+
+#3. formatting for list
+sports = ["Running", "Basketball"]
+print("My favorite sports are %s" %sports)
+
+#4. %f , %d, %s -- string  or any other objects represent a string
+pi = 3.1415926
+print("%.6f" %pi)
+
